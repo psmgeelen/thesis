455092b7e66d2c92034bcb01d3ab77a0a794ee63
psmgeelen <psm.geelen@student.maastrichtuniversity.nl>
2017-07-23 23:27:41 +0200

    first effort dynamic settings and naming of models through the use of a dataframe

--- basic models only.R
+++ basic models only.R
@@ -13,53 +13,57 @@ boxplot(bank$euribor3m)
 #Create artificial time-axis beforehand though. The website noted that the values were chronologi-
 #cally sorted. Therefore a simple itemnumber identifies a chronology. 
 time_axis <- as.numeric(rownames(bank))
-bank_time1 <- cbind(bank, time_axis)
-bank_time <- bank_time1[1:10000,]
+bank_time <- cbind(bank, time_axis)
+bank_time1 <- bank_time[1:10000,]
 
 #Set modeling techniques, for more information see description in rminer documentation
-models <- c("ctree", "ksvm", "mlpe", "lr")
+models_name <- rbind("Decision Tree", "Support Vector Machine", "Neural Network", "Naive", "LOGIT")
+models_specs <-rbind("ctree","ksvm","mlpe","naive","lr")
+models_additional_settings <- rbind(stuff)
+models <- cbind(models_name, models_specs)
+colnames(models) <- c("Name", "Specs")
 
 # Hold-out (train, test sets)
 H=holdout(bank_time$y,ratio=1/3)
 
 #----------------------Modeling----------------------------#
 
-for (i in models) {
 
-  M=fit(y~.,bank_time[H$tr,],model=i, task="class")
-  M2=fit(y~.,bank_time[H$tr,],model=i, task="prob")
+
+for (i in seq_len(nrow(models))) {
+
+  M=fit(y~.,bank_time[H$tr,], model = models[i,2],  task="class")
+  M2=fit(y~.,bank_time[H$tr,], model = models[i,2], task="prob")
   
   # Creating variables model
   P=predict(M,bank_time[H$ts,])
   P2=predict(M2,bank_time[H$ts,])
   
-  
-  #Title
-  cat(paste("----- Results for model", i, "-----"))
+    #Title
+  cat(paste("----- Results for model", models[i,1], "-----"))
   # Print stats. 
   # CONF Matrix 
-  print(paste("Confusion Matrix Predicition/Probability", i))
-  print(mmetric(bank_time$y[H$ts],P,"CONF"))
+  print(paste("Confusion Matrix Predicition/Probability", models[i,1] ))
   print(mmetric(bank_time$y[H$ts],P,"CONF"))
   # AUC of ROC
-  cat(paste("AUC for ROC for model", i, "= "))
+  cat(paste("AUC for ROC for model", models[i,1], "= "))
   cat(mmetric(bank_time$y[H$ts],P2,"AUC"), "\n")
   # AUC ALIFT
-  cat(paste("AUC for ALIFT model", i, "= "))
+  cat(paste("AUC for ALIFT model", models[i,1], "= "))
   cat(mmetric(bank_time$y[H$ts],P2,"ALIFT"), "\n")
-  # Accuracy
-  cat(paste("Accuracy Pred. model", i, "= "))
+    # Accuracy
+  cat(paste("Accuracy Pred. model", models[i,1], "= "))
   cat(mmetric(bank_time$y[H$ts],P,"ACC"), "\n")
-  cat(paste("Accuracy Prob. model", i, "= "))
-  cat(mmetric(bank_time$y[H$ts],P2,"ACC"), "\n") 
   
   #ROC
-  mgraph(bank_time$y[H$ts],P2,graph="ROC",TC=2,main=paste("ROC Curve for",i),
-       baseline=TRUE,leg=i,Grid=10)
+  mgraph(bank_time$y[H$ts],P2,graph="ROC",TC=2,main=paste("ROC Curve for",models[i,1]),
+       baseline=TRUE,leg=models[i,1],Grid=10)
   #LIFT
-  mgraph(bank_time$y[H$ts],P2,graph="LIFT",TC=2,main=paste("LIFT Curve for", i),
-       baseline=TRUE,leg=i,Grid=10)
-}
+  mgraph(bank_time$y[H$ts],P2,graph="LIFT",TC=2,main=paste("LIFT Curve for", models[i,1]),
+       baseline=TRUE,leg=models[i,1],Grid=10)
+  
+    }
+
 
 
 
