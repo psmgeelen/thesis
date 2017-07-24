149ff5a96956a00ee24a5de05e32dda562eb6043
psmgeelen <psm.geelen@student.maastrichtuniversity.nl>
2017-07-24 23:52:08 +0200

    sdf

--- basic models only.R
+++ basic models only.R
@@ -19,12 +19,12 @@ bank_time1 <- bank_time[1:10000,]
 #Set modeling techniques, for more information see description in rminer documentation
 models_name <- rbind("Decision Tree", "Support Vector Machine", "Neural Network", "Naive", "LOGIT")
 models_specs <-rbind("ctree","ksvm","mlpe","naive","lr")
-models_additional_settings <- rbind(stuff)
+#models_additional_settings <- rbind(stuff)
 models <- cbind(models_name, models_specs)
 colnames(models) <- c("Name", "Specs")
 
 # Hold-out (train, test sets)
-H=holdout(bank_time$y,ratio=1/3)
+H=holdout(bank_time$y,ratio=2/3)
 
 #----------------------Modeling----------------------------#
 
@@ -41,19 +41,34 @@ for (i in seq_len(nrow(models))) {
   
     #Title
   cat(paste("----- Results for model", models[i,1], "-----"))
-  # Print stats. 
-  # CONF Matrix 
-  print(paste("Confusion Matrix Predicition/Probability", models[i,1] ))
-  print(mmetric(bank_time$y[H$ts],P,"CONF"))
-  # AUC of ROC
-  cat(paste("AUC for ROC for model", models[i,1], "= "))
-  cat(mmetric(bank_time$y[H$ts],P2,"AUC"), "\n")
-  # AUC ALIFT
-  cat(paste("AUC for ALIFT model", models[i,1], "= "))
-  cat(mmetric(bank_time$y[H$ts],P2,"ALIFT"), "\n")
-    # Accuracy
-  cat(paste("Accuracy Pred. model", models[i,1], "= "))
-  cat(mmetric(bank_time$y[H$ts],P,"ACC"), "\n")
+  # Stats. 
+  #Perfomance measure
+  cat("---Rolling Window model", i, "with", c, "th iteration---", "\n")
+  C1=mmetric(data_ts$y,P,metric="AUC")
+  C2=mmetric(data_ts$y,P,metric="ALIFT")
+  C3=mmetric(data_ts$y,P,metric="ACC")
+  C4=mmetric(data_ts$y,P,metric="CONF")
+  C0=print(c)
+  
+  #Print findings
+  cat("AUC of", i, ":", C1, "\n")
+  cat("ALIFT of", i, ":", C2, "\n")
+  cat("ACC of", i, ":", C3, "\n")
+  cat("---Confusion Matrix---", "\n")
+  print(C4)  
+  
+  # Model label
+  C4 <- print(paste(i))
+  # Stack values modeling
+  C0_t <- c(C0_t, C0)
+  C1_t <- c(C1_t, C1)
+  C2_t <- c(C2_t, C2)
+  C3_t <- c(C3_t, C3)
+  C4_t <- c(C4_t, print(paste(i)))
+  C5_t <- c(C5_t, tr1)
+  C6_t <- c(C6_t, tr2)
+  C7_t <- c(C7_t, ts1)
+  C8_t <- c(C8_t, ts2)
   
   #ROC
   mgraph(bank_time$y[H$ts],P2,graph="ROC",TC=2,main=paste("ROC Curve for",models[i,1]),
